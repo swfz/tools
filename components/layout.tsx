a5fb6ee58eb176906ecef26ce543b27bbf779d19
swfz <sawafuji.09@gmail.com>
2021-11-26 17:03:52 +0900

    feat: 調整、tailwindcss導入

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,15 +7,17 @@ function Layout({children}) {
         <div className="flex justify-center text-3xl font-mono">
           <Link href="/">swfz tools</Link>
         </div>
-        <ul className="flex flex-col">
-          <li className="hover:text-pink-800 h-8">
-            <span className="font-bold text-lg">
-              <Link href="/timer">Picture in Piscture Timer</Link>
-            </span>
+        <ul className="flex flex-col p-2">
+          <li className="hover:text-pink-800 h-8 inline-block">
+            <a href="/timer" className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span>Picture in Piscture Timer</span>
+            </a>
           </li>
         </ul>
         <div className="absolute bottom-0">
-          @swfz
         </div>
       </div>
       <div className="p-8">
--- pages/timer/index.tsx
+++ pages/timer/index.tsx
@@ -142,37 +142,71 @@ const Timer: NextPage = () => {
         <h1 className="text-3xl">Picture in Picture Timer</h1>
       </div>
       <div className="p-3">
-        <div id="timer">
-          <canvas id="canvas" width="300" height="100" ref={canvasRef}></canvas>
-        </div>
         <div>
-          <span>Hour: </span>
-          <select className="form-select shadow-sm" value={formValue.hour} onChange={handleHourChange}>
-            {hours.map(hour => {
-              return <option key={hour} value={hour}>{hour.toString().padStart(2, '0')}</option>
-            })}
-          </select>
+          <div>
+            Picture in Pictureで表示できるタイマー
+          </div>
+          <div>
+            時間、分、秒を指定してタイマーを発動する
+          </div>
+          <div>
+            時間になると背景色が変わります
+          </div>
         </div>
-        <div>
-          <span>Min: </span>
-          <select className="form-select shadow-sm" value={formValue.min} onChange={handleMinChange}>
-            {minutes.map(min=> {
-              return <option key={min} value={min}>{min.toString().padStart(2, '0')}</option>
-            })}
-          </select>
+        <div className="divide-y divide-black-300 p-2">
+          <div id="timer">
+            <canvas id="canvas" width="300" height="100" ref={canvasRef}></canvas>
+          </div>
+          <div className="p-1 flex">
+            <span className="flex-1">Hour: </span>
+            <select className="flex-1 rounded" value={formValue.hour} onChange={handleHourChange}>
+              {hours.map(hour => {
+                return <option key={hour} value={hour}>{hour.toString().padStart(2, '0')}</option>
+              })}
+            </select>
+          </div>
+          <div className="p-1 flex">
+            <span className="flex-1">Minute: </span>
+            <select className="flex-1 rounded" value={formValue.min} onChange={handleMinChange}>
+              {minutes.map(min=> {
+                return <option key={min} value={min}>{min.toString().padStart(2, '0')}</option>
+              })}
+            </select>
+          </div>
+          <div className="p-1 flex">
+            <span className="flex-1">Second: </span>
+            <select className="flex-1 rounded" value={formValue.sec} onChange={handleSecChange}>
+              {seconds.map(sec=> {
+                return <option key={sec} value={sec}>{sec.toString().padStart(2, '0')}</option>
+              })}
+            </select>
+          </div>
+          <div className="p-1 flex flex-row">
+            <button className="flex items-center mx-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={startTimer}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              Start Timer
+            </button>
+            <button className="flex items-center mx-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={resetTimer}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z" />
+              </svg>
+              Reset Timer
+            </button>
+          </div>
+          <div className="p-1">
+            <button className="flex items-center mx-1 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={createVideo}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+              </svg>
+              Picture in Picture
+            </button>
+          </div>
+          <video muted={true} onLoadedMetadata={handleVideoEvent} ref={videoRef} className="hidden"></video>
         </div>
-        <div>
-          <span>Sec: </span>
-          <select className="form-select shadow-sm" value={formValue.sec} onChange={handleSecChange}>
-            {seconds.map(sec=> {
-              return <option key={sec} value={sec}>{sec.toString().padStart(2, '0')}</option>
-            })}
-          </select>
-        </div>
-        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={startTimer}>Start Timer</button>
-        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={resetTimer}>Reset Timer</button>
-        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={createVideo}>Picture in Picture</button>
-        <video muted={true} onLoadedMetadata={handleVideoEvent} ref={videoRef} className="hidden"></video>
       </div>
     </div>
   )
