bb6fd7069f1553be895fc18096f0dbc2c5fe24c7
swfz <sawafuji.09@gmail.com>
2022-07-12 02:59:51 +0900

    feat: kusa index

--- pages/kusa/index.tsx
+++ pages/kusa/index.tsx
@@ -6,6 +6,9 @@ const KusaIndex: NextPage = () => {
       <div>
         <p>Slackで展開すると草状況が閲覧できる</p>
         <p>"/kusa/[username]"でアクセスしてください</p>
+        <p>
+          コントリビューション画像は<a href="https://grass-graph.appspot.com/">grass-graph</a>を使わせてもらっています
+        </p>
       </div>
     </>
   );
