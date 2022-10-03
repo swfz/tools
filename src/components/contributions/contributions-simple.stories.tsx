dafce0e2b6cd3de9614c841541a09d93c1bf3998
swfz <sawafuji.09@gmail.com>
2022-10-04 03:35:35 +0900

    feat: #266  grouping

--- src/components/contributions/contributions-simple.stories.tsx
+++ src/components/contributions/contributions-simple.stories.tsx
@@ -8,7 +8,7 @@ export default {
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: 'ContributionsSimple',
+  title: 'kusa/ContributionsSimple',
   component: ContributionsSimple,
 } as ComponentMeta<typeof ContributionsSimple>;
 
