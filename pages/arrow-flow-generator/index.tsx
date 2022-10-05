46ce80156f811adfc738301af13955ad9680caf1
swfz <sawafuji.09@gmail.com>
2022-10-06 01:31:19 +0900

    feat: #272 スタイルの調整

--- pages/arrow-flow-generator/index.tsx
+++ pages/arrow-flow-generator/index.tsx
@@ -12,23 +12,21 @@ interface Item {
 }
 
 const ArrowFlowGenerator: NextPage = () => {
-  const defaultItem = {
+  const generateDefaultItem = () => ({
     id: uuid(),
-    name: '',
+    name: 'sample',
     fill: '#3399EE',
     stroke: '#666666',
     textSize: 20,
     textColor: '#FFFFFF',
-  }
-  const initialItems = [...Array(3)].map(() => defaultItem);
+  });
+
+  const initialItems = [...Array(3)].map(() => generateDefaultItem());
   const [items, setItems] = useState<Item[]>(initialItems);
 
   const handleAddButtonClick = (e: MouseEvent<HTMLElement>) => {
     setItems((prevItems) => {
-      return [
-        ...prevItems,
-        defaultItem,
-      ];
+      return [...prevItems, generateDefaultItem()];
     });
   };
 
@@ -47,10 +45,10 @@ const ArrowFlowGenerator: NextPage = () => {
   };
 
   const leftTopPadding = 10;
-  const topSideWidth = 100;
+  const topSideWidth = 150;
   const protrusionWidth = 30;
   const itemHeight = 200;
-  const itemWidth = 110;
+  const itemWidth = 160;
 
   return (
     <>
@@ -59,18 +57,23 @@ const ArrowFlowGenerator: NextPage = () => {
         <p>必要な数だけ項目を追加する</p>
         {items.map((item) => {
           return (
-            <span>
+            <span key={item.id} className="flex flex-row">
+              <label htmlFor="name">Name: </label>
               <input
-                key={item.id}
                 className="block w-32 appearance-none rounded border border-gray-500 bg-white px-1 py-0 leading-none text-gray-700 focus:outline-none"
                 onChange={handleInput(item, 'name')}
                 type="text"
                 placeholder="Name"
+                id="name"
               />
-              <input onChange={handleInput(item, 'fill')} value={item.fill} type="color" />
-              <input onChange={handleInput(item, 'stroke')} value={item.stroke} type="color" />
-              <input onChange={handleInput(item, 'textColor')} value={item.textColor} type="color" />
-              <input onChange={handleInput(item, 'textSize')} type="number" value={item.textSize} />
+              <label htmlFor="fill">Fill: </label>
+              <input onChange={handleInput(item, 'fill')} value={item.fill} type="color" id="fill" />
+              <label htmlFor="stroke">Stroke: </label>
+              <input onChange={handleInput(item, 'stroke')} value={item.stroke} type="color" id="stroke" />
+              <label htmlFor="textColor">Text: </label>
+              <input onChange={handleInput(item, 'textColor')} value={item.textColor} type="color" id="textColor" />
+              <label htmlFor="textSize">Text Size: </label>
+              <input onChange={handleInput(item, 'textSize')} type="number" value={item.textSize} id="textSize" />
             </span>
           );
         })}
@@ -83,13 +86,13 @@ const ArrowFlowGenerator: NextPage = () => {
           </button>
         </span>
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" width={800} height={500}>
+          <svg xmlns="http://www.w3.org/2000/svg" width={1200} height={500}>
             <defs>
               <polygon
                 id="first"
-                points={`0,0 ${topSideWidth},0 ${
-                  topSideWidth + protrusionWidth
-                },${topSideWidth} ${topSideWidth},${itemHeight} 0,${itemHeight}`}
+                points={`0,0 ${topSideWidth},0 ${topSideWidth + protrusionWidth},${
+                  itemHeight / 2
+                } ${topSideWidth},${itemHeight} 0,${itemHeight}`}
                 strokeWidth="3"
               />
               <polygon
@@ -118,7 +121,7 @@ const ArrowFlowGenerator: NextPage = () => {
             {items.map((item, i) => {
               const x =
                 i === 0 ? leftTopPadding + 10 + i * itemWidth : leftTopPadding + 10 + i * itemWidth + protrusionWidth;
-              const y = (leftTopPadding + itemHeight) / 2;
+              const y = leftTopPadding + (itemHeight / 2);
               return (
                 <text key={item.id} x={x} y={y} fontFamily="sans-serif" fontSize={item.textSize} fill={item.textColor}>
                   {item.name}
