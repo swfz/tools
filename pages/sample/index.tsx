713c123d2b474b7bc2ab8a9771d023e52d6a6c5d
swfz <sawafuji.09@gmail.com>
2022-10-15 17:26:11 +0900

    chore: add domain

--- pages/sample/index.tsx
+++ pages/sample/index.tsx
@@ -12,11 +12,11 @@ const Sample: NextPage = () => {
       <meta property="og:title" content="Sampele OG" />
       <meta property="og:description" content="Sample OG" />
       <meta property="og:site_name" content="swfz tools" />
-      <meta property="og:image" content="/api/og" />
+      <meta property="og:image" content="https://tools.swfz.io/api/og" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="/api/og" />
+      <meta name="twitter:image" content="https://tools.swfz.io/api/og" />
     </Head>
     <div>
       Sample
