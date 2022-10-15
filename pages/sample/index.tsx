8519c7697d44dc5969972e326d0f71b3f7d857db
swfz <sawafuji.09@gmail.com>
2022-10-15 17:18:23 +0900

    add og

--- pages/sample/index.tsx
+++ pages/sample/index.tsx
@@ -7,11 +7,16 @@ const Sample: NextPage = () => {
     <>
     <Head>
       <title>Hello world</title>
-      <meta
-        key="og:image"
-        property="og:image"
-        content="/api/og"
-      />
+      <meta property="og:url" content="/sample" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="Sampele OG" />
+      <meta property="og:description" content="Sample OG" />
+      <meta property="og:site_name" content="swfz tools" />
+      <meta property="og:image" content="/api/og" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="/api/og" />
     </Head>
     <div>
       Sample
