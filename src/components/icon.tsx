bc14a4341e678cfce16248adee9dda0eb8ee4599
swfz <sawafuji.09@gmail.com>
2022-10-15 17:56:44 +0900

    chore: fix tailwindcss order

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -127,7 +127,7 @@ export const ChevronDoubleIcon = () => (
     viewBox="0 0 24 24"
     strokeWidth="1.5"
     stroke="currentColor"
-    className="w-6 h-6"
+    className="h-6 w-6"
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
   </svg>
